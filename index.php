d70b4170dc9724888a8e7f0acc253cc87332fa8f
Vozikov Zhar <zvozikov@gmail.com>
2023-12-06 15:33:25 +0200

    HW_9_Factory_Method_Taxi

--- index.php
+++ index.php
@@ -1,30 +1,97 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
 
-interface Database
+interface TaxiFactory
 {
-    public function getData();
+    public function createTaxi(): Taxi;
 }
 
-class Mysql implements Database
+
+abstract class Taxi
 {
-    public function getData()
+    protected $model;
+
+    public function getModel()
     {
-        return 'some data from MySQL database';
+        return $this->model;
+    }
+
+    abstract public function getPrice();
+}
+
+
+class EconomyTaxi extends Taxi
+{
+    protected $model = 'Economy Model';
+
+    public function getPrice()
+    {
+        return 10.0;
     }
 }
 
-class Controller
+
+class StandardTaxi extends Taxi
 {
-    private $adapter;
+    protected $model = 'Standard Model';
 
-    public function __construct(Database $database)
+    public function getPrice()
     {
-        $this->adapter = $database;
-    }
-
-    public function getData()
-    {
-        return $this->adapter->getData();
+        return 20.0;
     }
 }
+
+
+class LuxuryTaxi extends Taxi
+{
+    protected $model = 'Luxury Model';
+
+    public function getPrice()
+    {
+        return 50.0;
+    }
+}
+
+
+class EconomyTaxiFactory implements TaxiFactory
+{
+    public function createTaxi(): Taxi
+    {
+        return new EconomyTaxi();
+    }
+}
+
+
+class StandardTaxiFactory implements TaxiFactory
+{
+    public function createTaxi(): Taxi
+    {
+        return new StandardTaxi();
+    }
+}
+
+
+class LuxuryTaxiFactory implements TaxiFactory
+{
+    public function createTaxi(): Taxi
+    {
+        return new LuxuryTaxi();
+    }
+}
+
+
+function orderTaxi(TaxiFactory $factory)
+{
+    $taxi = $factory->createTaxi();
+
+    echo "You ordered a {$taxi->getModel()} taxi. The price is {$taxi->getPrice()} USD.\n";
+}
+
+
+orderTaxi(new EconomyTaxiFactory());
+
+
+orderTaxi(new StandardTaxiFactory());
+
+
+orderTaxi(new LuxuryTaxiFactory());
